76a215f71f4147fcf4b92def16af1bb44a26f159
tk <tahia.khan@gmail.com>
2024-07-23 14:07:31 -0400

    clean up some power management settings

--- hardware/thinkpad-x1c.nix
+++ hardware/thinkpad-x1c.nix
@@ -1,15 +1,10 @@
-{ config, pkgs, lib, ... }:
+{ pkgs, lib, ... }:
 
 {
   hardware.cpu.intel.updateMicrocode = true;
   hardware.enableRedistributableFirmware = true; # Might help wifi?
 
-  # Backport from <nixos-hardware/lenovo/thinkpad/x1/6th-gen/QHD>
-  boot.kernelModules = [ "acpi_call" ];
-  boot.extraModulePackages = with config.boot.kernelPackages; [ acpi_call ];
-
   services.xserver.dpi = 210; # 210 is the native DPI of the HDR screen
-  # fonts.fontconfig.dpi = 140; # This is Xft.dpi in .Xresources, 140 = 210 / 1.5
 
   services.xserver.monitorSection = ''
     DisplaySize 310 174   # In millimeters
@@ -40,14 +35,12 @@
   };
 
   services.tlp.enable = true;
-  services.tlp.extraConfig = ''
-    CPU_SCALING_GOVERNOR_ON_AC=powersave
-    CPU_SCALING_GOVERNOR_ON_BAT=powersave
-    START_CHARGE_THRESH_BAT0=75
-    STOP_CHARGE_THRESH_BAT0=90
-    DEVICES_TO_DISABLE_ON_STARTUP="bluetooth"
-    DEVICES_TO_DISABLE_ON_BAT_NOT_IN_USE="bluetooth"
-  '';
+  services.tlp.settings = {
+    CPU_SCALING_GOVERNOR_ON_AC = "powersave";
+    CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+    START_CHARGE_THRESH_BAT0 = 75;
+    STOP_CHARGE_THRESH_BAT0 = 90;
+  };
 
   # Firmware updating
   services.fwupd.enable = true;
