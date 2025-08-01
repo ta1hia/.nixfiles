dc976afeb7dc13042380863ac5dc12f2ecdb4eda
tk <tahia.khan@gmail.com>
2025-07-31 22:21:03 -0400

    tmux copy paste stuff

--- home/terminal/tmux.nix
+++ home/terminal/tmux.nix
@@ -3,25 +3,51 @@
 {
   programs.tmux = {
     enable = true;
+
+    shortcut = "a";
     clock24 = true;
+    keyMode = "vi";
+    escapeTime = 1;
     plugins = with pkgs; [
-        #tmuxPlugins.continuum
         tmuxPlugins.resurrect
     ];
     sensibleOnTop = false;
-    extraConfig= '' # used for less common options, intelligently combines if defined in multiple places.
+
+    extraConfig= ''
+# used for less common options, intelligently combines if defined in multiple places.
+
 # panes
 set -g pane-border-style 'fg=colour240 bg=colour0'
 set -g pane-active-border-style 'bg=colour0 fg=colour245'
+set -g pane-base-index 1
 
 # statusbar
 set -g status-position bottom
+set -g status-justify centre
 set -g status-style 'bg=colour235 fg=colour28 dim'
+setw -g monitor-activity on
+setw -g window-status-activity-style 'fg=colour160'
+set -g visual-activity on
+
+# windows
+setw -g base-index 1
 
 # broadcast command
-bind a setw synchronize-panes
+bind A setw synchronize-panes
 setw -g window-status-current-format '#{?pane_synchronized,#[bg=red],}#I:#W'
 setw -g window-status-format         '#{?pane_synchronized,#[bg=red],}#I:#W'
+
+# copy mode
+bind-key -T copy-mode-vi 'v' send-keys -X begin-selection     # begin selection
+bind-key -T copy-mode-vi 'C-v' send-keys -X rectangle-toggle  # begin rectangle selection (C-v + Space)
+bind-key -T copy-mode-vi 'y' send-keys -X copy-selection      # yank selection
+
+# system clipboard (xclip)
+bind-key -T copy-mode-vi 'Y' send-keys -X copy-pipe-and-cancel "xclip -i -sel clipboard"
+bind C-v run "tmux set-buffer \"$(xclip -o -sel clipboard)\"; tmux paste-buffer"
+
+# reload
+bind r source-file ~/.config/tmux/tmux.conf \; display "reloaded from ~/.config/tmux/tmux.conf :)"
 
 set -g default-terminal "tmux-256color"
     ''; 
