627dfc71ceda55b962bdade1f1798a0795b30292
tk <tahia.khan@gmail.com>
2025-08-08 11:36:30 -0400

    nvim

--- home/neovim/default.nix
+++ home/neovim/default.nix
@@ -1,20 +1,41 @@
 { pkgs, ... }:
 
+let
+  treesitterLanguages = [
+    "c"
+    "go"
+    "lua"
+    "python"
+    "javascript"
+    "nix"
+    "rust"
+    "vim"
+    "vimdoc"
+    "query"
+  ];
+in
 {
   programs.neovim = {
     enable = true;
     defaultEditor = true;
-    plugins = with pkgs.vimPlugins; [
-      telescope-nvim
-      nvim-treesitter
-      vim-nix
-    ];
-    extraConfig = ''
-      set number
-      syntax on
-      set relativenumber
 
-      vim.opt.termguicolors = true
+    plugins = with pkgs.vimPlugins; [
+      # treesitter and friends
+      (nvim-treesitter.withPlugins (p: 
+        builtins.map (lang: builtins.getAttr ("tree-sitter-" + lang) p) treesitterLanguages
+      )) 
+    ];
+
+    extraLuaConfig = ''
+      require('nvim-treesitter.configs').setup {
+        highlight = {
+          enable = true,
+	  additional_vim_regex_highlighting = false,
+        },
+
+        indent = {
+          enable = true,
+        },
     '';
   };
   
