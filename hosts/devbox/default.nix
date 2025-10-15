76a8cdd88153c9424fdb1ae953cafb88a88f7da5
Tahia Khan <tkhan@parallelz.com>
2025-10-15 03:22:36 +0000

    ubuntu via system-manager stub

---
+++ hosts/devbox/default.nix
@@ -0,0 +1,12 @@
+{ config, pkgs, ... }:
+
+{
+  nixpkgs.hostPlatform = "aarch64-linux";
+
+  # This section configures system-wide packages.
+  # For example, we're installing a common package like htop.
+  environment.systemPackages = with pkgs; [
+    cowsay
+    htop
+  ];
+}
