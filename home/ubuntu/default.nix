61498bf39a80f08efb1b97aba1e4cdfb134ff259
Tahia Khan <tkhan@parallelz.com>
2025-10-16 02:33:39 +0000

    adding hm for tahia

--- home/ubuntu/default.nix
+++ home/ubuntu/default.nix
@@ -8,9 +8,7 @@
   programs.zsh = {
     initContent = ''
       bindkey '^R' history-incremental-search-backward
-
-      export PS1="%F{yellow}%B%n %F{magenta}%1~ %b%f$ "
-
+      export PS1="%{%F{124}%}%n%{%F{52}%}@%{%F{88}%}%m %{%F{253}%}%1~ %{%f%}$ "
       eval "$(/opt/homebrew/bin/brew shellenv)"
     '';
   }
