222d0e3a997b33ca426dbac322eaa8ec750453c9
Tahia Khan <tkhan@parallelz.com>
2025-10-16 03:30:28 +0000

    nvm system-manager, just a basic hm output for ubuntu

--- home/ubuntu/default.nix
+++ home/ubuntu/default.nix
@@ -5,11 +5,13 @@
     ../common
   ];
 
+  home.username = "tahia";
+  home.homeDirectory = "/home/tahia";
+
   programs.zsh = {
     initContent = ''
       bindkey '^R' history-incremental-search-backward
       export PS1="%{%F{124}%}%n%{%F{52}%}@%{%F{88}%}%m %{%F{253}%}%1~ %{%f%}$ "
-      eval "$(/opt/homebrew/bin/brew shellenv)"
     '';
-  }
+  };
 }
