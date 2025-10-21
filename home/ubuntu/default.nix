218a15062c73c2ae205d904168779475de456e35
tahia <tkhan@parallelz.com>
2025-10-21 01:27:38 +0000

    sops in ubuntu

--- home/ubuntu/default.nix
+++ home/ubuntu/default.nix
@@ -1,8 +1,9 @@
-{ ... }:
+{ inputs, ... }:
 
 {
   imports = [
     ../common
+    inputs.sops-nix.homeManagerModules.sops
   ];
 
   home.username = "tahia";
@@ -10,8 +11,14 @@
 
   programs.zsh = {
     initContent = ''
+      bindkey -e
       bindkey '^R' history-incremental-search-backward
       export PS1="%{%F{124}%}%n%{%F{52}%}@%{%F{88}%}%m %{%F{253}%}%1~ %{%f%}$ "
     '';
+  };
+
+  sops = {
+    age.keyFile = "/home/tahia/.config/sops/age/keys.txt";
+    defaultSopsFile = ./secrets.yaml;
   };
 }
